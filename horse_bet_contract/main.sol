8a9e3f70af98ab4f20633d0690e2ab1891fe43e7
maxallen687 <maxallen03919@gmail.com>
2023-05-11 19:56:15 +0530

    Add pargam directive and fix registerUser

--- horse_bet_contract/main.sol
+++ horse_bet_contract/main.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "horse_betting/storage.sol";
+import "./storage.sol";
 import "hardhat/console.sol";
 
 contract Main {
-    Storage public simpleStorage = new Storage();
+    Storage public simpleStorage = Storage(0x38cB7800C3Fddb8dda074C1c650A155154924C73);
 
     function acceptEther(uint256 amount, address _token) payable external {
        //logic amount = price X msg.value
@@ -23,16 +24,29 @@ contract Main {
    }
 
    function registerUser(address _user, uint _betAmount, uint _horse) public {
-       this.accept(_betAmount, _user);
+       // approve the transfer
+        address tokenAddress = 0xd9145CCE52D386f254917e481eB44e9943F39138;
+       IERC20 token = IERC20(tokenAddress);
+       console.log("Before transfer");
+       require(
+           token.allowance(msg.sender, address(this)) >= _betAmount,
+           "you have to approve control of tokens"
+       );
+       token.transferFrom(msg.sender, address(this), _betAmount);
+       console.log("After transfer");
        simpleStorage.registerUser(_user, _betAmount, _horse);
+        
    }
 
-
-   function returnToken() external {
+    
+   function returnToken() external view  {
        uint amount;
        address winner;
        (winner, amount) = simpleStorage.pickWinner();
-       IERC20 token = IERC20(winner);
-       token.transfer(msg.sender, amount);
+       address tokenAddress = 0xd9145CCE52D386f254917e481eB44e9943F39138;
+       IERC20 token = IERC20(tokenAddress);
+       console.log(winner, amount);
+    //    token.transfer(winner, amount);
+       console.log("Balance: %s", token.balanceOf(address(this)));
    }
 }
