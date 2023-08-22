1f509af0e4796b555ef4cd4ed4d9b73c6ad405f7
maxallen687 <maxallen03919@gmail.com>
2023-08-22 23:09:30 +0530

    Remove redundant imports, whitespaces and update winner picking logic

--- horse_bet_contract/service.sol
+++ horse_bet_contract/service.sol
@@ -1,34 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-
 /** @title Service Contract for Race 
 @author @amankr1279
 @notice This contract handles all mathematical operations related to a race.
  */
-import "hardhat/console.sol";
 
 contract Service {
     uint randNonce = 0;
 
-    /// @notice utility function that generates random number between 0 and HORSES-1(both inclusive)
+    /// @notice utility function that generates random number between 1 and HORSES(both inclusive)
     function random(uint HORSES) private returns (uint) {
         randNonce++;
-        return uint(keccak256(abi.encodePacked(block.timestamp,msg.sender,randNonce))) % HORSES;
+        uint x = uint(keccak256(abi.encodePacked(block.timestamp,msg.sender,randNonce))) % HORSES;
+        x = x+1;
+        return x;
     }
 
     function getRaceWinners(uint HORSES) public  returns (uint,uint,uint){
         uint h1 = random(HORSES);
-        uint h2 = 2;
-        uint h3 = 3;
-        // while (h2 != h1) 
-        // {
-        //     h2 = random(HORSES);
-        // }
-        // while (h3 != h2) 
-        // {
-        //     h3 = random(HORSES);
-        // }
+        uint h2 = random(HORSES);
+        uint h3 = random(HORSES);
+        while (h2 == h1) 
+        {
+            h2 = random(HORSES);
+        }
+        while (h3 == h2) 
+        {
+            h3 = random(HORSES);
+        }
+        while (h3 == h1) 
+        {
+            h3 = random(HORSES);
+        }
 
         return( h1, h2, h3);
     }
